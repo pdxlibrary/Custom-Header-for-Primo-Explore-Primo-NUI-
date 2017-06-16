757933e9c33d399bdc42c2a4163561cf7481412f
Mike Flakus <mflakus@pdx.edu>
2017-06-16 15:06:42 -0700

    removed more debug messages added comments

--- js/custom.js
+++ js/custom.js
@@ -12,9 +12,7 @@ var custom_header_template = "<div>CUSTOM HEADER GOES HERE</div>";
 			this.$onInit = function () {
 				angular.element(document).ready(function(){
 					var header_container = angular.element(document.getElementsByClassName('custom-header'));
-					if(header_container.length > 0)
-						console.log(" ### header exists when prmTopBarBefore controller initialized");
-					else
+					if(header_container.length == 0)
 						add_custom_header(header_container);
 				});
 			};
@@ -26,9 +24,9 @@ var custom_header_template = "<div>CUSTOM HEADER GOES HERE</div>";
 
 function add_custom_header(header_container)
 {
-	var header_container = angular.element(document.getElementsByClassName('custom-header'));
 	if(header_container.length == 0)
 	{
+		// move custom header to top of standard page
 		var custom_header_html = '<div class="custom-header">'+custom_header_template+'</div>'
 		var prm_explore_main = angular.element(document.querySelector('prm-explore-main'));
 		if(prm_explore_main.length == 1)
@@ -38,6 +36,7 @@ function add_custom_header(header_container)
 			angular.element(header_container).after(prm_explore_main);
 		}
 		
+		// move custom header to top of record page
 		var prm_full_view_page = angular.element(document.querySelector('prm-full-view-page'));
 		if(prm_full_view_page.length == 1)
 		{
@@ -45,10 +44,5 @@ function add_custom_header(header_container)
 			var header_container = angular.element(document.getElementsByClassName('custom-header'));
 			header_container.after(prm_full_view_page);
 		}
-	}
-	else
-	{
-		console.log("ERROR: header already exists - this function shouldn't have been called");
-		console.log("ERROR: path name: " + window.location.pathname);
 	}
 }
